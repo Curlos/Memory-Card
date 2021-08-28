68dbeb90d15913b2bcf2f2fe8bd63b9a8651a37a
Carlos Martinez <curlosmart@gmail.com>
2021-08-28 18:46:19 -0400

    set cards based on level and pick randomly

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './style.css';
 const App = () => {
   const [currentScore, setCurrentScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
-  const [level, setLevel] = useState(1)
+  const [level, setLevel] = useState(4)
   const [currentCards, setCurrentCards] = useState({
     cardNums: [],
     cardSrcs: []
@@ -36,26 +36,40 @@ const App = () => {
   }
 
   const setCards = () => {
-    // while (currentCards.length < numOfCards[level])
-    for (let i = 0; i < numOfCards[level]; i++) {
+    let pickedCards = 0
+    const newCardNums = [...currentCards.cardNums]
+    const newCardSrcs = [...currentCards.cardSrcs]
+    
+    while (pickedCards < numOfCards[level]) {
       const cardNum = getRandomInt(1, 300)
-      const cardNumStr = (cardNum).toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})
+      if (!newCardNums.includes(cardNum)) {
+        const cardNumStr = (cardNum).toLocaleString('en-US', {minimumIntegerDigits: 3, useGrouping:false})
 
-      console.log(`CURRENT CARD NUMS: ${currentCards['cardNums']}`)
-      
-      const cardSrc = `/assets/2K-Cards/${cardNumStr}.png`
-      console.log(cardSrc)
-      setCurrentCards({ cardNums: [...currentCards.cardNums, cardNum]})
-      setCurrentCards({ cardSrcs: [...currentCards.cardSrcs, cardSrc]})
+        console.log(`CURRENT CARD NUMS: ${currentCards['cardNums']}`)
+        console.log(currentCards)
+        
+        const cardSrc = `/assets/2K-Cards/${cardNumStr}.png`
+
+        newCardNums.push(cardNum)
+        newCardSrcs.push(cardSrc)
+        pickedCards += 1
+      }
     }
+
+    setCurrentCards({
+      cardNums: newCardNums,
+      cardSrcs: newCardSrcs
+    })
   }
+
+  console.log(currentCards)
 
 
   return (
     <div className="mainContainer">
       <Header />
       <Scoreboard currentScore={currentScore} bestScore={bestScore}/>
-      <Cards currentCards={currentCards} handlePickCard={handlePickCard}/>
+      <Cards currentCards={currentCards} numOfCards={numOfCards} level={level} handlePickCard={handlePickCard}/>
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,15 @@
 import Card from './Card'
 
-const Cards = ({ currentCards, handlePickCard }) => {
+const Cards = ({ currentCards, numOfCards, level, handlePickCard }) => {
+
+  if (!currentCards.cardNums) {
+    return "Loading..."
+  }
   
   return (
     <div className="cardsContainer">
        <div className="cards">
-        {Array.from({length: 60}, (x, i) => {
+        {Array.from({length: numOfCards[level]}, (x, i) => {
           const cardNum = currentCards['cardNums'][i]
           const src = currentCards['cardSrcs'][i]
           return (
